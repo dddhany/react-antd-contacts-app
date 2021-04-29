ad006a884b40de822a623e7f6a868a3dc7a1341a
Dhany <dhany@ecodi.co>
2021-04-29 17:36:46 +0800

    add drawer and tester

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('render addContactButton in App component', () => {
+  const { queryByTestId } = render(<App />);
+  const addContactButton = queryByTestId("add-contact-button")
+  expect(addContactButton).toBeInTheDocument();
+});
+
+test('drawer component not in App component on render', () => {
+  const { queryByTestId } = render(<App />);
+  const addContactDrawer = queryByTestId("add-contact-drawer")
+  expect(addContactDrawer).not.toBeInTheDocument();
+});
+
+test('render drawer component not in App component on render', () => {
+  const { queryByTestId } = render(<App />);
+  const addContactButton = queryByTestId("add-contact-button")
+  fireEvent.click(addContactButton)
+
+  const addContactDrawer = queryByTestId("add-contact-drawer")
+  expect(addContactDrawer).toBeInTheDocument()
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React, { FC } from 'react';
-import { Button } from 'antd'
-import './App.less';
+import React, { FC, Fragment, useState } from "react";
+import { Button } from "antd";
+import "./App.less";
 import { PlusCircleFilled } from "@ant-design/icons";
+import AddDrawer from "./AddDrawer";
 
-const App: FC = () => (
-  <div className="App">
-    <Button type="primary" icon={<PlusCircleFilled/>} >Button</Button>
-  </div>
-);
+const App: FC = () => {
+  const [showDrawer, setShowDrawer] = useState(false);
+  return (
+    <Fragment>
+      <Button
+        type="primary"
+        icon={<PlusCircleFilled />}
+        data-testid="add-contact-button"
+        onClick={() => setShowDrawer(true)}
+      >
+        Button
+      </Button>
+      <AddDrawer show={showDrawer} handleOnClose={() => setShowDrawer(false)} />
+    </Fragment>
+  );
+};
 
 export default App;
