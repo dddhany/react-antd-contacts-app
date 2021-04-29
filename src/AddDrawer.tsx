7b8d6ade082be0c01b84f1b6f2d4555cd79ee35a
Dhany <dhany@ecodi.co>
2021-04-29 22:56:08 +0800

    up to video 3

--- src/AddDrawer.tsx
+++ src/AddDrawer.tsx
@@ -1,17 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Drawer } from "antd";
+import { Drawer, Form, Button, Input } from "antd";
 
-const AddDrawer = ({ show, handleOnClose }) => {
+const AddDrawer = ({
+  show,
+  handleOnClose,
+  handleOnFinish,
+  handleOnFinishFailed,
+}) => {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+  };
+  const [form] = Form.useForm();
+  const [, forceUpdate] = useState({});
+
+  //to disable submit button at the beginning
+  useEffect(() => {
+    forceUpdate({});
+  }, []);
+
   return (
     <Drawer
+      width={412}
       data-testid="add-contact-drawer"
       title="Add Contact"
       visible={show}
       onClose={handleOnClose}
       maskClosable={false}
     >
-      <h1>Content Here</h1>
+      <Form
+        form={form}
+        name="basic"
+        initialValues={initialValues}
+        onFinish={handleOnFinish}
+        onFinishFailed={handleOnFinishFailed}
+      >
+        <Form.Item
+          label="First Name"
+          name="firstName"
+          rules={[{ required: true, message: "Please input your first name!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Last Name"
+          name="lastName"
+          rules={[{ required: true, message: "Please input your last name!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Phone Number"
+          name="phoneNumber"
+          rules={[
+            { required: true, message: "Please input your phone number!" },
+          ]}
+        >
+          <Input type="tel" />
+        </Form.Item>
+
+        <Form.Item shouldUpdate>
+          {() => (
+            <Button
+              type="primary"
+              htmlType="submit"
+              disabled={
+                !form.isFieldsTouched(true) ||
+                !!form.getFieldsError().filter(({ errors }) => errors.length)
+                  .length
+              }
+            >
+              Add
+            </Button>
+          )}
+        </Form.Item>
+      </Form>
     </Drawer>
   );
 };
@@ -19,6 +84,8 @@ const AddDrawer = ({ show, handleOnClose }) => {
 AddDrawer.propTypes = {
   show: PropTypes.bool.isRequired,
   handleOnClose: PropTypes.func.isRequired,
+  handleOnFinish: PropTypes.func.isRequired,
+  handleOnFinishFailed: PropTypes.func.isRequired,
 };
 
 export default AddDrawer;
--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,17 @@ import AddDrawer from "./AddDrawer";
 
 const App: FC = () => {
   const [showDrawer, setShowDrawer] = useState(false);
+  const [values, setValues] = useState({});
+  const [errorInfo, setErrorInfo] = useState({});
+
+  const handleAddFormOnFinish = (values) => {
+    setValues(values)
+    console.log("values: ", values)
+    console.log("errorInfo: ", errorInfo)
+  }
+  const handleAddFormOnFinishFailed = (errorInfo) => {
+    setErrorInfo(errorInfo)
+  }
   return (
     <Fragment>
       <Button
@@ -16,7 +27,9 @@ const App: FC = () => {
       >
         Button
       </Button>
-      <AddDrawer show={showDrawer} handleOnClose={() => setShowDrawer(false)} />
+      <AddDrawer show={showDrawer} handleOnClose={() => setShowDrawer(false)}
+        handleOnFinish={handleAddFormOnFinish}
+        handleOnFinishFailed={ handleAddFormOnFinishFailed}/>
     </Fragment>
   );
 };
